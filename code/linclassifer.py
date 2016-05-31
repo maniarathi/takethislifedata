359cc7c83fa96ec0855d06c1502f800ab787fece
Arathi Mani <mani.arathi@gmail.com>
2016-05-31 11:52:07 -0700

    Added some more feature experiments

--- code/linclassifer.py
+++ code/linclassifer.py
@@ -1,25 +1,30 @@
 glove_home = 'glove.6B'
 
 import os
+import unicodedata
+import utils
+from nltk.stem import *
 from collections import Counter, defaultdict
 import numpy as np
 from sklearn.cross_validation import train_test_split
 from sklearn.manifold import TSNE
-from sklearn.metrics import classification_report, accuracy_score, f1_score
-import utils
+from sklearn.metrics import f1_score, classification_report
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.neighbors import NearestNeighbors
 from sklearn import svm
 from sklearn.linear_model import SGDClassifier
 from sklearn.feature_extraction import DictVectorizer
+from vaderSentiment.vaderSentiment import sentiment as vaderSentiment
 
 GLOVE = utils.glove2dict(os.path.join(glove_home, 'glove.6B.100d.txt'))
+featured_vocab_file = '../featured_vocab.txt'
 
 def plotTSNE(data):
     model = TSNE(n_components=2, random_state=0)
     np.set_printoptions(suppress=True)
-    tsne_data = model.fit_transform(data) 
+    tsne_data = model.fit_transform(data)
 
     x1, x2 = zip(*tsne_data)
 
@@ -40,22 +45,27 @@ def plotTSNE(data):
 
 def binary_class_func(y):
     if y in (1, 2):
-        return 1
+        return 0
     elif y in (4, 5):
-        return 2
+        return 1
     else:
         return None
-    
-def ternary_class_func(y):       
-    if y in (0, 1):
-        return 1
-    elif y in (3, 4):
-        return 3
-    else:
+
+def ternary_class_func(y):
+    if y in (1, 2):
+        return 0
+    elif y in (4, 5):
         return 2
+    elif y in (3):
+        return 1
+    else:
+        return None
 
 def original_class(y):
-    return y
+    if y in (1, 2, 3, 4, 5):
+        return y-1
+    else:
+        return None
 
 ###############################################################################
 # BEGIN FEATURE EXTRACTORS HERE
@@ -80,7 +90,66 @@ def glove_phi(texts):
 def unigram_phi(texts):
     all_feats = []
     for text in texts:
-        all_feats.append(Counter(text))
+        all_feats.append(Counter(text.split(" ")))
+    vectorizer = DictVectorizer(sparse=False)
+    return vectorizer.fit_transform(all_feats)
+
+def stemmed_unigram_phi(texts):
+    all_feats = []
+    stemmer = PorterStemmer()
+    for text in texts:
+        words = text.split(" ")
+        clean_words = []
+        for word in words:
+            word = unicodedata.normalize('NFKD',unicode(word,"ISO-8859-1")).encode("ascii","ignore")
+            clean_words.append(word.lower())
+        stemmed_words = [stemmer.stem(word) for word in clean_words]
+        all_feats.append(Counter(stemmed_words))
+    vectorizer = DictVectorizer(sparse=False)
+    return vectorizer.fit_transform(all_feats)
+
+def stemmed_featured_unigrams_phi(texts):
+    all_feats = []
+    stemmer = PorterStemmer()
+    f = open(featured_vocab_file, 'r')
+    stemmed_featured_words = set([])
+    for line in f:
+        word = line.strip()
+        word = unicodedata.normalize('NFKD',unicode(word,"ISO-8859-1")).encode("ascii","ignore")
+        stemmed_featured_words.add(stemmer.stem(word))
+    for text in texts:
+        words = text.split(" ")
+        clean_words = []
+        for word in words:
+            word = unicodedata.normalize('NFKD',unicode(word,"ISO-8859-1")).encode("ascii","ignore")
+            clean_words.append(word.lower())
+        stemmed_words = [stemmer.stem(word) for word in clean_words if stemmer.stem(word) in stemmed_featured_words]
+        all_feats.append(Counter(stemmed_words))
+    vectorizer = DictVectorizer(sparse=False)
+    return vectorizer.fit_transform(all_feats)
+
+def stemmed_featured_unigrams_phi_plus_sentiment(texts):
+    all_feats = []
+    stemmer = PorterStemmer()
+    f = open(featured_vocab_file, 'r')
+    stemmed_featured_words = set([])
+    for line in f:
+        word = line.strip()
+        word = unicodedata.normalize('NFKD',unicode(word,"ISO-8859-1")).encode("ascii","ignore")
+        stemmed_featured_words.add(stemmer.stem(word))
+    for text in texts:
+        words = text.split(" ")
+        clean_words = []
+        for word in words:
+            word = unicodedata.normalize('NFKD',unicode(word,"ISO-8859-1")).encode("ascii","ignore")
+            clean_words.append(word.lower())
+        stemmed_words = [stemmer.stem(word) for word in clean_words if stemmer.stem(word) in stemmed_featured_words]
+        text_features = Counter(stemmed_words)
+        vs = vaderSentiment(text)
+        text_features['neg_sentiment'] = vs['neg']
+        text_features['neu_sentiment'] = vs['neu']
+        text_features['pos_sentiment'] = vs['pos']
+        all_feats.append(text_features)
     vectorizer = DictVectorizer(sparse=False)
     return vectorizer.fit_transform(all_feats)
 
@@ -98,7 +167,7 @@ def bigram_phi(texts):
 
 ###############################################################################
 # END FEATURE EXTRACTORS HERE
-###############################################################################   
+###############################################################################
 
 def get_labeled_data(filename='../final_rated_posts.csv', class_func=original_class):
     with open(filename, 'rb') as inpFile:
@@ -116,19 +185,20 @@ def get_labeled_data(filename='../final_rated_posts.csv', class_func=original_cl
     print "Size of all labeled data (train + test) = %d" % len(responses)
     return responses, labels
 
-def build_dataset(phi, class_func):
-    responses, labels = get_labeled_data(class_func=class_func)
-    data = phi(responses)
-    return data, labels
+def build_dataset(posts, phi, class_func):
+    data = phi(posts)
+    return data
 
-def run_experiment(model, phi=glove_phi, class_func=original_class):
-    X, y = build_dataset(phi, class_func)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+def run_experiment(posts, labels, model, phi=glove_phi, class_func=original_class):
+    X = build_dataset(posts, phi, class_func)
+    y = labels
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=500, random_state=42)
     score, predicted_labels = model(X_train, X_test, y_train, y_test)
     print "Ran %s" % model.__name__
-    print "Accuracy on test set: %f" % score
-    print "F1 Score of test set: " 
-    print f1_score(y_test, predicted_labels, average='micro')
+    #print "Accuracy on test set: %f" % score
+    #print "F1 Score of test set: "
+    #print f1_score(y_test, predicted_labels, average='micro')
+    print classification_report(y_test, predicted_labels)
 
 ###############################################################################
 # BEGIN MODELS HERE
@@ -162,18 +232,40 @@ def SGD(X_train, X_test, y_train, y_test):
     sgd_score = mod.score(X_test, y_test)
     return sgd_score, sgd_labels
 
+def NaiveBayes(X_train, X_test, y_train, y_test):
+    mod = MultinomialNB()
+    mod.fit(X_train, y_train)
+    nb_labels = mod.predict(X_test)
+    nb_score = mod.score(X_test, y_test)
+    return nb_score, nb_labels
 
 ###############################################################################
 # END MODELS HERE
 ###############################################################################
 
 if __name__ == "__main__":
-    #run_experiment(k_means)
-    #run_experiment(SVC)
-    #run_experiment(SGD)
-    #run_experiment(k_means, phi=unigram_phi)
-    #run_experiment(SVC, phi=unigram_phi)
-    #run_experiment(SGD, phi=unigram_phi)
-    run_experiment(k_means, phi=bigram_phi)
-    run_experiment(SVC, phi=bigram_phi)
-    run_experiment(SGD, phi=bigram_phi)
+    posts, labels = get_labeled_data(class_func=original_class)
+    #run_experiment(posts, labels, k_means)
+    #run_experiment(posts, labels, SVC)
+    #run_experiment(posts, labels, SGD)
+    #run_experiment(posts, labels, NaiveBayes)
+    #run_experiment(posts, labels, k_means, phi=unigram_phi)
+    #run_experiment(posts, labels, SVC, phi=unigram_phi)
+    #run_experiment(posts, labels, SGD, phi=unigram_phi)
+    #run_experiment(posts, labels, NaiveBayes, phi=unigram_phi)
+    #run_experiment(posts, labels, k_means, phi=stemmed_unigram_phi)
+    #run_experiment(posts, labels, SVC, phi=stemmed_unigram_phi)
+    #run_experiment(posts, labels, SGD, phi=stemmed_unigram_phi)
+    #run_experiment(posts, labels, NaiveBayes, phi=stemmed_unigram_phi)
+    #run_experiment(posts, labels, k_means, phi=stemmed_featured_unigrams_phi)
+    #run_experiment(posts, labels, SVC, phi=stemmed_featured_unigrams_phi)
+    #run_experiment(posts, labels, SGD, phi=stemmed_featured_unigrams_phi)
+    #run_experiment(posts, labels, NaiveBayes, phi=stemmed_featured_unigrams_phi)
+    run_experiment(posts, labels, k_means, phi=stemmed_featured_unigrams_phi_plus_sentiment)
+    run_experiment(posts, labels, SVC, phi=stemmed_featured_unigrams_phi_plus_sentiment)
+    run_experiment(posts, labels, SGD, phi=stemmed_featured_unigrams_phi_plus_sentiment)
+    run_experiment(posts, labels, NaiveBayes, phi=stemmed_featured_unigrams_phi_plus_sentiment)
+    #run_experiment(posts, labels, k_means, phi=bigram_phi)
+    #run_experiment(posts, labels, SVC, phi=bigram_phi)
+    #run_experiment(posts, labels, SGD, phi=bigram_phi)
+    #run_experiment(posts, labels, NaiveBayes, phi=bigram_phi)
--- statistics.py
+++ statistics.py
@@ -1,21 +1,26 @@
 import operator
+from nltk.stem import *
 
-f = open('posts.csv', 'r')
+f = open('final_rated_posts.csv', 'r')
 vocab = dict()
 num = 0
 for line in f:
-	if len(line) <= 1:
-		num += 1
-	else:
-		words = line.strip().split()
-		for word in words:
-			word = word.lower()
-			if word in vocab:
-				vocab[word] += 1
-			else:
-				vocab[word] = 1
+	num += 1
+	attributes = line.split('@@')
+	words = attributes[1].strip().split()
+	for word in words:
+		word = word.lower()
+		if word in vocab:
+			vocab[word] += 1
+		else:
+			vocab[word] = 1
 f.close()
 print "Number of lines: %d" % num
 vocab = sorted(vocab.items(), key=operator.itemgetter(1), reverse=True)
 for word in vocab:
 	print word[0] + '\t' + str(word[1])
+f = open('featured_vocab.txt', 'w')
+for word in vocab:
+	if word[1] > 100 and word[1] < 10000:
+		f.write(word[0] + '\n')
+f.close()
